327e0970bef503fdb6126cc9f5d78c40bae78743
Ojifu <ahmedhijazi49@gmail.com>
2023-11-17 13:09:26 -0700

    lab 4
    Added and completed lab 4

--- App.jsx
+++ App.jsx
@@ -1,20 +1,46 @@
 import React, { useState } from 'react';
-import ToDoList from './ToDoList'; // Import the ToDoList component
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import ToDoList from './ToDoList'; 
+import ToDoForm from './ToDoForm'; 
 
 function App() {
-  // Initialize the state with an array of tasks
+  // Initial state of tasks
   const [tasks, setTasks] = useState([
-    'Do laundry',
-    'Go to gym',
-    'Walk dog'
+    { id: 1, text: 'Do laundry', completed: true },
+    { id: 2, text: 'Go to gym', completed: false },
+    { id: 3, text: 'Walk dog', completed: true },
   ]);
 
+  // Function to add a new task
+  const addTask = (taskText) => {
+    const newId = tasks.length ? tasks[tasks.length - 1].id + 1 : 1;
+    const newTask = { id: newId, text: taskText, completed: false }; 
+    setTasks([...tasks, newTask]); 
+  };
+
+  // Function to toggle the completion status
+  const toggleCompletion = (taskId) => {
+    setTasks(tasks.map(task => 
+      task.id === taskId ? { ...task, completed: !task.completed } : task
+    ));
+  };
+
   return (
-    <><SafeAreaView>
-      <ToDoList tasks={tasks} />
-      <ToDoForm />
-    </SafeAreaView></>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <ToDoList tasks={tasks} toggleCompletion={toggleCompletion} />
+      </ScrollView>
+      <ToDoForm addTask={addTask} />
+    </SafeAreaView>
   );
 }
+
+// Styles for the app
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    marginTop: 20, 
+  },
+});
 
 export default App;
