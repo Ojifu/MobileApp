2760c2c5fe674a23125b670f7c15965999059275
Ojifu <ahmedhijazi49@gmail.com>
2023-11-23 16:12:44 -0700

    lab 5 steps 1-4
    lab 5 steps 1-4 added, not tested and need to do step 5/maybe 6

--- App.jsx
+++ App.jsx
@@ -1,46 +1,21 @@
-import React, { useState } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
-import ToDoList from './ToDoList'; 
-import ToDoForm from './ToDoForm'; 
+// App.jsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import AboutScreen from './AboutScreen';
+
+const Stack = createStackNavigator();
 
 function App() {
-  // Initial state of tasks
-  const [tasks, setTasks] = useState([
-    { id: 1, text: 'Do laundry', completed: true },
-    { id: 2, text: 'Go to gym', completed: false },
-    { id: 3, text: 'Walk dog', completed: true },
-  ]);
-
-  // Function to add a new task
-  const addTask = (taskText) => {
-    const newId = tasks.length ? tasks[tasks.length - 1].id + 1 : 1;
-    const newTask = { id: newId, text: taskText, completed: false }; 
-    setTasks([...tasks, newTask]); 
-  };
-
-  // Function to toggle the completion status
-  const toggleCompletion = (taskId) => {
-    setTasks(tasks.map(task => 
-      task.id === taskId ? { ...task, completed: !task.completed } : task
-    ));
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <ToDoList tasks={tasks} toggleCompletion={toggleCompletion} />
-      </ScrollView>
-      <ToDoForm addTask={addTask} />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-// Styles for the app
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    marginTop: 20, 
-  },
-});
 
 export default App;
